c46947cbcab3bc153f2d9c5908b14d59f75829cf
Cody Duskin <codyduskin@me.com>
2017-07-26 19:58:40 -0600

    crud

--- app.js
+++ app.js
@@ -1,22 +1,102 @@
-$(document).ready(()=>{
+$(document).ready(() => {})
+const url = 'http://localhost:3000/notes'
+$.get(url, function(cardsData) {
+  makeCards(cardsData)
 })
 
-$.get('https://tranquil-tundra-87782.herokuapp.com/notes',(data)=>{
-  console.log(data);
-  for (var i = 0; i < data.length; i++) {
-          $('.card-group').append(
-            `<div class="card card-outline-warning mb-3 text-center">
+$('#addNote').click(function() {
+  sendPostRequest()
+})
+$('#editNote').click(function() {
+  sendPutRequest()
+})
+
+$('.card-group').click(function(event) {
+
+  const destroy = $(event.target).attr('data-id')
+  $.ajax({
+    url: url + '/' + destroy,
+    type: 'DELETE',
+    success: function(result) {
+      // Do something with the result
+      $(`#card${destroy}`).fadeOut()
+      $(`#span${destroy}`).fadeOut()
+    }
+  })
+})
+
+function makeCards(cardsData) {
+  for (var i = 0; i < cardsData.length; i++) {
+    $('.card-group').append(
+      `<div class="card card-outline-warning mb-3 text-center" id="card${cardsData[i].id}">
               <div class="card-block" style="height: 40vh;">
               <div class ="top">
-              <button type="button" data-id="${data[i].id}" class="close" aria-label="Close">
-              <span aria-hidden="true">&times;</span> </button>
+              <button type="button" data-id="${cardsData[i].id}" class="close" id="delete${cardsData[i].id}"aria-label="Close">
+              <span aria-hidden="true" data-id="${cardsData[i].id}" id="span${cardsData[i].id}">&times;</span> </button>
               </div>
-              <h4 class="card-title">${data[i].fam}</h4>
-              <p class="card-text">${data[i].note} </p>
-              <p class="card-text"><small class="text-muted">${data[i].priority}<br>${data[i].date.slice(0,10)}</p></small>
+              <h4 class="card-title">${cardsData[i].fam}</h4>
+              <p class="card-text">${cardsData[i].note} </p>
+              <p class="card-text"><small class="text-muted">${cardsData[i].priority}<br>${cardsData[i].date.slice(0,10)}</p></small>
               <div class="footerButton" style="">
             </div>
             </div>`
-          )
+    )
   }
-})
+}
+
+function sendPostRequest(event) {
+  let recipientForm = $('.recipientForm').val()
+  let noteForm = $('.noteForm').val()
+  let dateForm = $('.dateForm').val()
+  let priorityForm = $('.priorityForm').val()
+  var newPost = {
+    note: noteForm,
+    fam: recipientForm,
+    date: dateForm,
+    priority: priorityForm
+  }
+  $.post(url, newPost)
+    .then((resPost) => {
+      $.get(url + '/' + resPost, function(cardsData) {
+        $('.card-group').append(
+          `<div class="card card-outline-warning mb-3 text-center" id="card${cardsData.id}">
+          <div class="card-block" style="height: 40vh;">
+          <div class ="top">
+          <button type="button" data-id="${cardsData.id}" class="close" id="delete${cardsData.id}"aria-label="Close">
+          <span aria-hidden="true" data-id="${cardsData.id}" id="span${cardsData.id}">&times;</span> </button>
+          </div>
+          <h4 class="card-title">${cardsData.fam}</h4>
+          <p class="card-text">${cardsData.note} </p>
+          <p class="card-text"><small class="text-muted">${cardsData.priority}<br>${cardsData.date.slice(0,10)}</p></small>
+          <div class="footerButton" style="">
+        </div>
+        </div>`
+        )
+      })
+    })
+}
+function sendPutRequest(event) {
+  let editId = $('.noteId').val()
+  let recipientFormPut = $('.recipientForm2').val()
+  let noteFormPut = $('.noteForm2').val()
+  let dateFormPut = $('.dateForm2').val()
+  let priorityFormPut = $('.priorityForm2').val()
+  var newPost2 = {
+    note: noteFormPut,
+    fam: recipientFormPut,
+    date: dateFormPut,
+    priority: priorityFormPut
+  }
+  console.log(editId);
+  $.ajax({
+    url: url + '/' + editId,
+    type: 'PUT',
+    data: newPost2,
+    success: function(result) {
+      // Do something with the result
+
+    }
+  })
+
+
+}
